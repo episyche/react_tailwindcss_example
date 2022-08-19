296910b9a082cb3ab1886eff2751bdf18c108f9f
sivasuriyan-p <sivasuriyan58@gmail.com>
2022-08-19 15:20:44 +0530

    configure tailwind in reactjs

---
+++ src/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <p className="mx-16 my-16 text-xl font-bold text-green-600 cursor-pointer">
+    Configure Tailwind CSS with React.JS
+    </p>
+  );
+}
